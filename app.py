062b1edfde98cf3c2280939c0a36971b53e79488
Samuel <sam3bett@gmail.com>
2025-10-31 09:41:46 +0300

    added route

--- app.py
+++ app.py
@@ -45,13 +45,7 @@ register_routes(app)
 def root():
     return {"message": "GreenShelf API running "}
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
